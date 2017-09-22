ce4d8784a79fe9bb03df2a78ad966159cc9051a3
t.kaneko <nontan20xx@gmail.com>
2017-09-22 19:12:24 +0900

    Initial commit

---
+++ app/scripts.babel/background.js
@@ -0,0 +1,15 @@
+
+const REAL_WORLD = 'realworld.jp';
+const MORI = 'mrga.service-navi.jp';
+
+chrome.browserAction.onClicked.addListener(function(tab)
+{
+	if (tab.url.indexOf(REAL_WORLD) != -1)
+	{
+		chrome.tabs.sendMessage(tab.id, 'real_world');
+	}
+	else if (tab.url.indexOf(MORI) != -1)
+	{
+		chrome.tabs.sendMessage(tab.id, 'mori');
+	}
+});
---
+++ app/scripts.babel/mori.js
@@ -0,0 +1,104 @@
+import $ from 'jquery';
+
+let page_num = 1;
+let cnt = 0;
+let links = [];
+
+const MORI = 'mrga.service-navi.jp';
+/*
+const REAL_LINKS_KEY = 'real_links';
+const REAL_PAGE_KEY = 'real_world_page';
+const REAL_WORLD_PAGE = 'http://realworld.jp/contents/rec/page/';
+*/
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse)
+{
+	if (request == "mori")
+	{
+		//set_storage(REAL_PAGE_KEY, 1);
+		alert('morimori');
+	}
+});
+/*
+$(function(){
+	if (location.host.indexOf(REAL_WORLD) == -1)
+	{
+		return false;
+	}
+	chrome.storage.local.get(REAL_LINKS_KEY, function(value)
+	{
+		if ($.isEmptyObject(value))
+		{
+			return;
+		}
+		read_article(JSON.parse(value[REAL_LINKS_KEY]));
+	});
+	chrome.storage.local.get(REAL_PAGE_KEY, function(value)
+	{
+		if ($.isEmptyObject(value))
+		{
+			return;
+		}
+		get_real_world(value[REAL_PAGE_KEY]);
+	});
+});
+
+function get_real_world(value)
+{
+	page_num = value;
+	var a_list = $("section#recommend").find('a.clearfix.sec-recommend.able');
+	$.each(a_list, function(i, val) {
+		if (cnt >= 5)
+		{
+			return false;
+		}
+		var href = $(val).attr('href');
+		links.push(href);
+		cnt++;
+	});
+	if (cnt >= 5)
+	{
+		chrome.storage.local.remove(REAL_PAGE_KEY);
+		go_article(links);
+	}
+	else
+	{
+		page_num++;
+		set_storage(REAL_PAGE_KEY, page_num);
+		window.location.href = REAL_WORLD_PAGE + String(page_num);
+	}
+}
+
+function read_article(articles)
+{
+	window.scroll(0, $(document).height());
+	setTimeout(function(){
+		var tag = $("section#complete").find("a.modal-close");
+		$(tag)[0].click();
+	}, 1000);
+
+	if (articles.length <= 0)
+	{
+		chrome.storage.local.remove(REAL_LINKS_KEY);
+		window.close();
+		return false;
+	}
+	setTimeout(function(){
+		go_article(articles);
+	}, 2000);
+}
+
+function go_article(articles)
+{
+	var url = articles.shift();
+	set_storage(REAL_LINKS_KEY, JSON.stringify(articles));
+	window.location.href = url;
+}
+
+function set_storage(key, value)
+{
+	var entity = {};
+	entity[key] = value;
+	chrome.storage.local.set(entity);
+}
+*/
